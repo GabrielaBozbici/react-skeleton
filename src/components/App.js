6fe98452dda296fa9b6aca2ef7d9efdc88c4b673
Gabriela Bozbici <gabriela@bozbici.com>
2016-11-23 09:47:11 +0200

    changed the location of app.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import List from './List.jsx';
+import '../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
